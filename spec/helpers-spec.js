92cda93f8bae5d5252b08d777053e3be69813e14
steelbrain <de.steelbrain@gmail.com>
2016-02-14 10:57:28 +0000

    :new: Add specs for helper

---
+++ spec/helpers-spec.js
@@ -0,0 +1,35 @@
+'use babel'
+
+import {visitSource} from '../lib/helpers'
+
+describe('Helpers', function() {
+
+  beforeEach(function() {
+    atom.workspace.destroyActivePaneItem()
+  })
+
+  describe('visitSource', function() {
+    it('opens the file and sets cursor position', function() {
+      const filePath = __filename
+      waitsForPromise(function() {
+        return visitSource(filePath, [0, 1]).then(function() {
+          const activeEditor = atom.workspace.getActiveTextEditor()
+          expect(activeEditor.getPath()).toBe(filePath)
+          expect(activeEditor.getCursorBufferPosition()).toEqual([0, 1])
+        })
+      })
+    })
+
+    it('opens the file and does not set cursor position if its not provided', function() {
+      const filePath = __filename
+      waitsForPromise(function() {
+        return visitSource(filePath, [0, 0]).then(function() {
+          const activeEditor = atom.workspace.getActiveTextEditor()
+          expect(activeEditor.getPath()).toBe(filePath)
+          expect(activeEditor.getCursorBufferPosition()).toEqual([0, 0])
+        })
+      })
+    })
+  })
+
+})
